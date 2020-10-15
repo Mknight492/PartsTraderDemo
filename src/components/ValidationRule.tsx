6a6b4e024ccc3add022f202a810f13c7cbc43e68
Mknight492 <39689669+Mknight492@users.noreply.github.com>
2020-10-15 22:04:28 +1300

    added ticks and crosses

--- src/components/ValidationRule.tsx
+++ src/components/ValidationRule.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
+import { Check } from "./Check";
+import { Cross } from "./Cross";
 
 export const ValidationRule = ({ value, validationFunction, message }: any) => {
   return (
     <div>
-      {validationFunction(value) ? "pass" : "fail"} {message}
+      {validationFunction(value) ? <Check /> : <Cross />}
+      {message}
     </div>
   );
 };
