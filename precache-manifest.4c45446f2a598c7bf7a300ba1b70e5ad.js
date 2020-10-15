f9a4bb864d79d393d7ebd87460988d65eaebfc41
Mknight492 <39689669+Mknight492@users.noreply.github.com>
2020-10-15 21:39:57 +1300

    Updates

---
+++ precache-manifest.4c45446f2a598c7bf7a300ba1b70e5ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c272299357c01e78f7077607a8d2e9d",
+    "url": "/PartsTraderDemo/index.html"
+  },
+  {
+    "revision": "660cd97dac5cd3a6f282",
+    "url": "/PartsTraderDemo/static/css/main.61788106.chunk.css"
+  },
+  {
+    "revision": "705bea5f9a8ea2c1dc6f",
+    "url": "/PartsTraderDemo/static/js/2.99c24b8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PartsTraderDemo/static/js/2.99c24b8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660cd97dac5cd3a6f282",
+    "url": "/PartsTraderDemo/static/js/main.69cecfc6.chunk.js"
+  },
+  {
+    "revision": "66dee5a772e65933de0a",
+    "url": "/PartsTraderDemo/static/js/runtime-main.ab1f721e.js"
+  }
+]);
